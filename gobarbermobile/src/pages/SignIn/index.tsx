6b90700f6f8c669060e126945c855df9b20f2656
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2020-09-02 22:21:33 -0300

    Form usability, unform, signin and signup

--- gobarbermobile/src/pages/SignIn/index.tsx
+++ gobarbermobile/src/pages/SignIn/index.tsx
@@ -1,16 +1,77 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
-import { Container, Title } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const navigation = useNavigation<any>();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
+
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Log on GoBarber</Title>
-    </Container>
+    <>
+      <KeyboardAvoidingView enabled style={{ flex: 1}}behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Log on GoBarber</Title>
+            </View>
+            <Form onSubmit={handleSignIn} ref={formRef}>
+              <Input
+                name="email"
+                icon="mail"
+                placeholder="Email"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Password"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {formRef.current.submitForm()}}
+              />
+
+              <Button onPress={() => {formRef.current.submitForm()}}>Go</Button>
+            </Form>
+
+            <ForgotPassword onPress={() => (console.log(data))}>
+              <ForgotPasswordText>Password Recover</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create new account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 }
 
--- gobarbermobile/src/pages/SignUp/index.tsx
+++ gobarbermobile/src/pages/SignUp/index.tsx
@@ -1,10 +1,79 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title, BackToSignInText, BackToSignIn } from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const navigation = useNavigation<any>();
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
   return (
-    <Container />
+    <>
+      <KeyboardAvoidingView enabled style={{ flex: 1}}behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Create your account</Title>
+            </View>
+            <Form onSubmit={() => {}} ref={formRef}>
+              <Input
+                name="name"
+                icon="user"
+                placeholder="Name"
+                autoCapitalize="words"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current.focus()
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                name="email"
+                icon="mail"
+                placeholder="Email"
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current.focus()
+                }}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Password"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => formRef.current.submitForm()}
+              />
+
+              <Button onPress={() => formRef.current.submitForm()}>Go</Button>
+            </Form>
+          </Container>
+        </ScrollView>
+
+      </KeyboardAvoidingView>
+
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Back to logon</BackToSignInText>
+      </BackToSignIn>
+    </>
   );
 }
 
